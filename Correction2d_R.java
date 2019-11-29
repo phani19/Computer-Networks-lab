7fb1824bea713b7e3e805f16e9d8ffb361e7dda9
phani19 <47142847+phani19@users.noreply.github.com>
2019-11-29 06:43:05 +0530

    Update Correction2d_R.java

--- Correction2d_R.java
+++ Correction2d_R.java
@@ -1,48 +1,54 @@
-import java.io.*;
-import java.net.*;
-
-class Correction2d_R
+import javax.crypto.spec.*;
+import java.security.*;
+import javax.crypto.*;
+  
+public class Main
 {
-public static void main(String args[])throws Exception
-{
-ServerSocket ss= new ServerSocket(5555);
-System.out.println("waiting for connection");
-Socket soc=ss.accept();
-System .out.println("connection established");
-BufferedReader br =new BufferedReader(new InputStreamReader(soc.getInputStream()));
-int n= Integer.parseInt(br.readLine());
-int i,j,inp[][]=new int[n+1][9];
-
-for(i=0;i<=n;i++)
-{
-for(j=0;j<=8;j++)
-{
-inp[i][j]=Integer.parseInt(br.readLine());
-}
-}
-int sum,x=0,y=0;
-for(i=0;i<n+1;i++)
-{sum=0;
-for(j=0;j<9;j++)
-{sum+=inp[i][j];}
-if(sum%2==1){x=i;break;}
-}
-
-for(i=0;i<9;i++)
-{sum=0;
-for(j=0;j<n+1;j++)
-{sum+=inp[j][i];}
-if(sum%2==1){y=i;break;}
-}
-
-inp[x][y]=(inp[x][y]+1)%2;
-//-----------------------------
-for(i=0;i<n;i++)
-{
-for(j=0;j<8;j++)
-System.out.print(inp[i][j]);
-System.out.println();
-}//------------------------
-System.out.println("error detected and corrected at row "+(x+1)+" column "+(y+1));
-}//end of main
+   private static String algorithm = "RC4";
+  
+   public static void main(String []args) throws Exception {
+      String toEncrypt = "The shorter you live, the longer you're dead!";
+  
+      System.out.println("Encrypting...");
+      byte[] encrypted = encrypt(toEncrypt, "password");
+  
+      System.out.println("Decrypting...");
+      String decrypted = decrypt(encrypted, "password");
+     
+      System.out.println("Decrypted text: " + decrypted);
+   }
+  
+   public static byte[] encrypt(String toEncrypt, String key) throws Exception {
+      // create a binary key from the argument key (seed)
+      SecureRandom sr = new SecureRandom(key.getBytes());
+      KeyGenerator kg = KeyGenerator.getInstance(algorithm);
+      kg.init(sr);
+      SecretKey sk = kg.generateKey();
+  
+      // create an instance of cipher
+      Cipher cipher = Cipher.getInstance(algorithm);
+  
+      // initialize the cipher with the key
+      cipher.init(Cipher.ENCRYPT_MODE, sk);
+  
+      // enctypt!
+      byte[] encrypted = cipher.doFinal(toEncrypt.getBytes());
+  
+      return encrypted;
+   }
+  
+   public static String decrypt(byte[] toDecrypt, String key) throws Exception {
+      // create a binary key from the argument key (seed)
+      SecureRandom sr = new SecureRandom(key.getBytes());
+      KeyGenerator kg = KeyGenerator.getInstance(algorithm);
+      kg.init(sr);
+      SecretKey sk = kg.generateKey();
+  
+      // do the decryption with that key
+      Cipher cipher = Cipher.getInstance(algorithm);
+      cipher.init(Cipher.DECRYPT_MODE, sk);
+      byte[] decrypted = cipher.doFinal(toDecrypt);
+  
+      return new String(decrypted);
+   }
 }

c2f201e3de0752b4590464e6f56b3ee0041621f7
phani19 <47142847+phani19@users.noreply.github.com>
2019-11-29 06:41:17 +0530

    Update Client_PARITY1.java

--- Client_PARITY1.java
+++ Client_PARITY1.java
@@ -1,43 +1,32 @@
-import java.net.*;
-import java.util.*;
-import java.io.*;
-import java.util.Random; 
-class Client_PARITY1
+class Diffie_Hellman
 {
-public static void main(String args[])
-{
-try{
-Socket soc=new Socket("127.0.0.1",5000);
-System.out.println("connected");String input="",Str="";
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-BufferedReader in=new BufferedReader(new InputStreamReader(soc.getInputStream()));
-PrintWriter out=new PrintWriter(soc.getOutputStream(),true);
-while(!input.equals("Over")){
-System.out.print("Client:");
-input=br.readLine();
-//even parity
-int i,cnt=0;
-for(i=0;i<input.length();i++)
-{
-if((input.charAt(i))=='1'){cnt++;}
+ public static void main(String args[])
+ {
+ Scanner sc=new Scanner(System.in);
+ System.out.println("Enter modulo(p)");
+ int p=sc.nextInt();
+ System.out.println("Enter primitive root of "+p);
+ int g=sc.nextInt();
+ System.out.println("Choose 1st secret no(Alice)");
+ int a=sc.nextInt();
+ System.out.println("Choose 2nd secret no(BOB)");
+ int b=sc.nextInt();
+ 
+ int A = (int)Math.pow(g,a)%p;
+ int B = (int)Math.pow(g,b)%p;
+ 
+ int S_A = (int)Math.pow(B,a)%p;
+ int S_B =(int)Math.pow(A,b)%p; 
+ 
+ if(S_A==S_B)
+ {
+ System.out.println("ALice and Bob can communicate with each other!!!");
+ System.out.println("They share a secret no = "+S_A); 
+ }
+ 
+ else
+ {
+ System.out.println("ALice and Bob cannot communicate with each other!!!");
+ }
+ } 
 }
-if(cnt%2==1)
-{input=input+'1';}
-if(cnt%2==0)
-{input=input+'0';}
-//adding error
-Random rand=new Random();
-int index=rand.nextInt(input.length());
-
-StringBuilder input1 = new StringBuilder(input);
-input1.setCharAt(index, '1');
-input=input1.toString();
-//
-out.println(input);
-Str=in.readLine();
-System.out.println("Server:"+Str);
-}
-}
-catch(Exception e)
-{System.out.println(e);}
-}}
